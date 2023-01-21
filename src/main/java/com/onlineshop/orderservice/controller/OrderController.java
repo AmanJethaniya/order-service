939e2c1a538fc73ed218643463a77c39503e9a6f
Aman Jethaniya <amanjethaniya619@gmail.com>
2023-01-21 16:27:39 +0530

    Added db connection

--- src/main/java/com/onlineshop/orderservice/controller/OrderController.java
+++ src/main/java/com/onlineshop/orderservice/controller/OrderController.java
@@ -1,18 +1,23 @@
 package com.onlineshop.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onlineshop.orderservice.model.OrderRequest;
+import com.onlineshop.orderservice.service.OrderService;
 
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
+	@Autowired
+	private OrderService service;
 	
 	@PostMapping
 	public String placeOrder(@RequestBody OrderRequest orderRequest) {
+		service.placeOrder(orderRequest);
 		return "Order Placed successfully";
 		
 	}
